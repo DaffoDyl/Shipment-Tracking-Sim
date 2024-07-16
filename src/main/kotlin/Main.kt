bb02b54e55bb7d4483bd9bd20312def2cc26d947
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 17:24:10 -0600

    Modify imports

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window

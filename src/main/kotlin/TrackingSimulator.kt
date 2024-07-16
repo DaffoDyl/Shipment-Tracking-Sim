30302f712ff59370bfb2db626b445d3e04857616
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 02:22:14 -0600

    Init TrackingSimulator

--- src/main/kotlin/TrackingSimulator.kt
+++ src/main/kotlin/TrackingSimulator.kt
@@ -1,2 +1,14 @@
 class TrackingSimulator {
+    private val shipments: MutableList<Shipment> = mutableListOf()
+
+    fun findShipment(id: String) : Shipment? {
+        for (shipment in shipments) {
+            if (shipment.id == id) {
+                return shipment
+            }
+        }
+        return null
+    }
+    fun addShipments() {}
+    fun runSimulation() {}
 }

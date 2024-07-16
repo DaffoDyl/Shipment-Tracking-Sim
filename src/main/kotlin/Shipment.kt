d62eb774a96a9c4ad40ab0a44370bb8d8b8de98f
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 02:14:51 -0600

    Init Shipment

--- src/main/kotlin/Shipment.kt
+++ src/main/kotlin/Shipment.kt
@@ -1,2 +1,13 @@
-class Shipment {
+class Shipment(val id: String, timeStamp : Long) {
+    private val notes: MutableList<String> = mutableListOf()
+    private val updateHistory: MutableList<ShippingUpdate> = mutableListOf()
+    private var status: String = "created"
+    private var expectedDeliveryDateTimestamp: Long = 0
+    private var currentLocation: String = ""
+
+    init {
+        addUpdate(ShippingUpdate("", status, timeStamp))
+    }
+    fun addNote(note: String) { notes.add(note) }
+    fun addUpdate(update: ShippingUpdate) { updateHistory.add(update) }
 }

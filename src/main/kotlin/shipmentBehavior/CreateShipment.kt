1f9d5f384251dae3e2fcd4f1049d5dbf477db651
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 05:13:35 -0600

    Implement CreateShipment

--- src/main/kotlin/shipmentBehavior/CreateShipment.kt
+++ src/main/kotlin/shipmentBehavior/CreateShipment.kt
@@ -1,9 +1,12 @@
 package shipmentBehavior
 
 import Shipment
+import ShippingUpdate
 
 class CreateShipment: ShipmentBehavior {
     override fun updateShipment(shipment: Shipment?, update: List<String>) {
-        TODO("Not yet implemented")
+        val status: String = update[0]
+        val timestamp: Long = update[2].toLong()
+        shipment?.addUpdate(ShippingUpdate("", status, timestamp))
     }
 }

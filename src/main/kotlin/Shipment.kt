61c74405deb4e9f89e17f4eb4ba4ca23b6d6ab9c
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 04:47:07 -0600

    Update shipment setters

--- src/main/kotlin/Shipment.kt
+++ src/main/kotlin/Shipment.kt
@@ -1,19 +1,23 @@
-class Shipment(private val id: String, timeStamp : Long) {
+class Shipment(
+    private var id: String,
+    private var status: String,
+) {
+    private var currentLocation: String = ""
+    private var expectedDeliveryDateTimestamp: Long = 0
     private val notes: MutableList<String> = mutableListOf()
     private val updateHistory: MutableList<ShippingUpdate> = mutableListOf()
-    private var status: String = "created"
-    private var expectedDeliveryDateTimestamp: Long = 0
-    private var currentLocation: String = ""
 
-    init {
-        addUpdate(ShippingUpdate("", status, timeStamp))
-    }
+
     fun getId(): String { return id }
+    fun setId(id: String) { this.id = id }
+    fun getStatus(): String { return status }
+    fun setStatus(status: String) { this.status = status }
+    fun getCurrentLocation(): String { return currentLocation }
+    fun setCurrentLocation(location: String) { currentLocation = location }
+    fun getExpectedDeliveryDateTimestamp(): Long { return expectedDeliveryDateTimestamp }
+    fun setExpectedDeliveryDateTimestamp(timestamp: Long) {expectedDeliveryDateTimestamp = timestamp}
     fun getNotes() : List<String> { return notes }
     fun getHistory() : List<ShippingUpdate> { return updateHistory }
-    fun getStatus(): String { return status }
-    fun getExpectedDeliveryDateTimestamp(): Long { return expectedDeliveryDateTimestamp }
-    fun getCurrentLocation(): String { return currentLocation }
 
     fun addNote(note: String) { notes.add(note) }
     fun addUpdate(update: ShippingUpdate) { updateHistory.add(update) }

facb6538c1b004d42751ca13dcd3929ff068a4b0
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 05:17:01 -0600

    Implement DelayShipment

--- src/main/kotlin/shipmentBehavior/DelayShipment.kt
+++ src/main/kotlin/shipmentBehavior/DelayShipment.kt
@@ -1,9 +1,15 @@
 package shipmentBehavior
 
 import Shipment
+import ShippingUpdate
 
 class DelayShipment: ShipmentBehavior {
     override fun updateShipment(shipment: Shipment?, update: List<String>) {
-        TODO("Not yet implemented")
+        val status: String = update[0]
+        val timestamp: Long = update[2].toLong()
+        val expectedTimestamp: Long = update[3].toLong()
+        shipment?.addUpdate(ShippingUpdate(shipment.getStatus(), status, timestamp))
+        shipment?.setStatus(status)
+        shipment?.setExpectedDeliveryTimestamp(expectedTimestamp)
     }
 }

16583dba46d04ecaf7d294c6d3758a9d34bea9d0
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 21:44:48 -0600

    Use list of id's for view helper

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -17,7 +18,7 @@ import kotlinx.coroutines.launch
 @Preview
 fun App() {
     var inputText by remember { mutableStateOf("") }
-    val viewHelper = remember { mutableStateListOf<TrackerViewHelper>() }
+    val viewHelper = remember { mutableStateListOf<String>() }
     val coroutineScope = rememberCoroutineScope()
     coroutineScope.launch { trackingSim.runSimulation() }
 
@@ -31,36 +32,50 @@ fun App() {
                     modifier = Modifier.weight(1f)
                 )
                 Button(
-                    onClick = {
-
-                        viewHelper.add(TrackerViewHelper(Shipment(inputText, "dummy"))) },
+                    onClick = { viewHelper.add(inputText) },
                     modifier = Modifier.height(height = 56.dp)
                 ) {
                     Text("Track")
                 }
             }
-            for (view in viewHelper) {
+            for (id in viewHelper) {
+                val ship = trackingSim.findShipment(id)
                 Column {
-                    Row {
-                        Text("Tracking shipment: ${view.shipmentId}")
-                        Button(
-                            onClick = {},
-                        ) {
-                            Text("X")
+                    if (ship != null) {
+                        val view = TrackerViewHelper(ship)
+                        Row {
+                            Text("Tracking shipment: ${view.shipmentId}")
+                            Button(
+                                onClick = { viewHelper.remove(id) },
+                                modifier = Modifier.padding(4.dp)
+                            ) {
+                                Text("X")
+                            }
+                        }
+                        Text("Status: ${view.shipmentStatus}")
+                        Text("Location: ${view.shipmentLocation}")
+                        Text("Expected Delivery: ${view.expectedShipmentDeliveryDate}")
+                        Text("Status Updates:")
+                        for (update in view.shipmentUpdateHistory) {
+                            Text(update)
+                        }
+                        Text("Notes:")
+                        for (note in view.shipmentNotes) {
+                            Text(note)
+                        }
+                    } else {
+                        Row {
+                            Text("Tracking shipment: $id does not exist")
+                            Button(
+                                onClick = { viewHelper.remove(id) },
+                                modifier = Modifier.padding(4.dp)
+                            ) {
+                                Text("X")
+                            }
                         }
                     }
-                    Text("Status: ${view.shipmentStatus}")
-                    Text("Location: ${view.shipmentLocation}")
-                    Text("Expected Delivery: ${view.expectedShipmentDeliveryDate}")
-                    Text("Status Updates:")
-                    for (update in view.shipmentUpdateHistory) {
-                        Text(update)
-                    }
-                    Text("Notes:")
-                    for (note in view.shipmentNotes) {
-                        Text(note)
-                    }
                 }
+
             }
         }
 

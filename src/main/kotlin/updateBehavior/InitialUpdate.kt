4382de5a0d2944052ed065811a9d2a90c07b228b
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-16 21:43:26 -0600

    Fix id/status

--- src/main/kotlin/updateBehavior/InitialUpdate.kt
+++ src/main/kotlin/updateBehavior/InitialUpdate.kt
@@ -7,6 +7,6 @@ class InitialUpdate: UpdateBehavior {
     override fun updateShipment(shipment: Shipment?, update: List<String>) {
         val status: String = update[0]
         val timestamp: Long = update[2].toLong()
-        shipment?.addUpdate(ShippingUpdate("", status, timestamp))
+        shipment?.addUpdate(ShippingUpdate("not created", status, timestamp))
     }
 }

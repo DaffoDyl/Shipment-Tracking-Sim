57ac0fa3549554120f242e55425241bfb6401823
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-23 20:48:13 -0600

    Give field better name

--- src/main/kotlin/ShipmentPacker.kt
+++ src/main/kotlin/ShipmentPacker.kt
@@ -2,10 +2,10 @@ import shipmentTypes.*
 
 class ShipmentPacker {
 
-    fun packShipment(update: List<String>): Shipment {
-        val status = update[0]
-        val id = update[1]
-        val type = update[3]
+    fun packShipment(shipmentInfo: List<String>): Shipment {
+        val status = shipmentInfo[0]
+        val id = shipmentInfo[1]
+        val type = shipmentInfo[3]
         val shipment = when(type) {
             "standard" -> Shipment(id, status)
             "express" -> ExpressShipment(id, status)

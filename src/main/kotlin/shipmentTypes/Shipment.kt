ec2f4c626fc62d9e0e3d1182d3215a4a266d7299
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-23 20:39:11 -0600

    Fix missing observer update

--- src/main/kotlin/shipmentTypes/Shipment.kt
+++ src/main/kotlin/shipmentTypes/Shipment.kt
@@ -4,20 +4,33 @@ import ShipmentObserver
 import ShipmentSubject
 import ShippingUpdate
 
-open class Shipment(
+open class Shipment(id: String, status: String): ShipmentSubject {
 
-    var id: String,
-
-    var status: String
-
-): ShipmentSubject {
-
+    var id: String = id
+        set(value){
+            field = value
+            notifyObservers()
+        }
+    var status: String = status
+        set(value){
+            field = value
+            notifyObservers()
+        }
     var currentLocation: String = "unknown"
+        set(value){
+            field = value
+            notifyObservers()
+        }
 
     var expectedDeliveryTimestamp: Long = 0
+        set(value){
+            field = value
+            notifyObservers()
+        }
 
     var notes: MutableList<String> = mutableListOf()
         private set
+
     var updateHistory: MutableList<ShippingUpdate> = mutableListOf()
         private set
 
